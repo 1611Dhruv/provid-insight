06badc4206e9ae1c4dabd27f4d09f75c352fe51c
Dhruv Desai <ddesai7@wisc.edu>
2023-11-11 14:30:18 -0600

    I am done with life pt2

---
+++ app/record/page.js
@@ -0,0 +1,9 @@
+import Recorder from "@/components/Recorder";
+
+export default function RecordingPage() {
+  return (
+    <div>
+      <Recorder />
+    </div>
+  );
+}
---
+++ components/Recorder.jsx
@@ -0,0 +1,7 @@
+export default function Recorder() {
+  return (
+    <div className="w-[90%] bg-">
+      <input type="file" hidden></input>
+    </div>
+  );
+}

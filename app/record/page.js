6c04921ef5caab1babb030b1225067509e8854af
Dhruv Desai <ddesai7@wisc.edu>
2023-11-11 16:17:57 -0600

    Somehting i dont know

--- app/record/page.js
+++ app/record/page.js
@@ -1,8 +1,8 @@
-import Recorder from "@/components/Recorder";
+import Recorder from "@/components/VideoRecorder";
 
 export default function RecordingPage() {
   return (
-    <div>
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <Recorder />
     </div>
   );

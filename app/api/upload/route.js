53b3470f6cbed9ef5f12f7f27ff38f02fab4d833
Dhruv Desai <ddesai7@wisc.edu>
2023-11-12 01:30:03 -0600

    Thomas

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import mongoose from "mongoose";
+import mongodb from "mongodb";
+import { Readable } from "stream";
 import User from "@/model/User";
 
 export async function POST(req) {
@@ -10,27 +11,16 @@ export async function POST(req) {
   if (!file) {
     return Response.json({ success: true });
   }
-  console.log(file.name);
 
-  let client = null;
-  try {
-    client = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to db");
-  } catch (err) {
-    console.log(err);
-    return new NextResponse("Internal Server Error");
-  }
+  const bucket = new mongodb.GridFSBucket();
+  const id = new mongodb.ObjectId();
 
-  try {
-    console.log(User);
-    await User.create({
-      name: name,
-      email: email,
-    });
-    console.log("Message sent");
-    return new NextResponse("Success");
-  } catch (err) {
-    console.log(err);
-    return new NextResponse("Internal Server Error");
-  }
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+  const stream = Readable.from(buffer);
+  stream.pipe(
+    bucket.openUploadStreamWithId(id, {
+      metadata: { field: "myField", value: "myValue" },
+    })
+  );
 }

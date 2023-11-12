04622789094616b782bfd9c25b6079426e33b4c4
Thomas Liang <lliang.thomas@gmail.com>
2023-11-12 01:41:11 -0600

    route:)

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,26 +1,38 @@
 import { NextResponse } from "next/server";
-import mongodb from "mongodb";
 import { Readable } from "stream";
-import User from "@/model/User";
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const url = "mongodb+srv://lliangthomas:1JXpWCXDBSoZOp0S@madhackscluster.9ecahxo.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+const dbName = "MadHacksDB";
 
 export async function POST(req) {
-  const data = await req.formData();
-  console.log(data);
-  const file = data.get("file");
-  const { name, email } = JSON.parse(data.get("user"));
-  if (!file) {
+  try {
+    const data = await req.formData();
+    console.log(data);
+    const file = data.get("file");
+    const { email } = JSON.parse(data.get("user"));
+    if (!file) {
+      return Response.json({ success: true });
+    }
+  
+    await client.connect();
+    const db = client.db(dbName);
+    const bucket = new mongodb.GridFSBucket(db);
+    const id = new mongodb.ObjectId();
+
+    
+  
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+    const stream = Readable.from(buffer);
+    stream.pipe(bucket.openUploadStreamWithId(id, {
+        metadata: { field: 'myField', value: 'myValue' }
+    }));
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
     return Response.json({ success: true });
   }
-
-  const bucket = new mongodb.GridFSBucket();
-  const id = new mongodb.ObjectId();
-
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-  const stream = Readable.from(buffer);
-  stream.pipe(
-    bucket.openUploadStreamWithId(id, {
-      metadata: { field: "myField", value: "myValue" },
-    })
-  );
 }
--- model/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-});
-
-const User = mongoose.models.User || mongoose.model("User", UserSchema);
-export default User;

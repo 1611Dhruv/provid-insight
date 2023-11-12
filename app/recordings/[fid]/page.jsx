0e2dbb6d68e843a8363fd7d327fa4cd9c216a8ac
Dhruv Desai <ddesai7@wisc.edu>
2023-11-12 11:27:32 -0600

    Done with life

--- app/recordings/[fid]/page.jsx
+++ app/recordings/[fid]/page.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import ScrollableComponent from "@/components/ScrollingComponent";
 import BouncingDotsLoader from "@/components/BouncingDotsLoader";
+import BarGraph from "@/components/BarChart";
 
 export default function ViewResult({ params }) {
   // const url = "/compressed.mp4"; // updated to have fid
@@ -69,70 +70,68 @@ export default function ViewResult({ params }) {
     }
   };
 
-  return;
-  {
-    loading === 0 ? (
-      <div className="mt-10 flex flex-col items-center">
-        <div className="grid grid-cols-1 md:grid-cols-3 w-11/12 p-4 mx-auto text-center">
-          <div className="mockup-browser border bg-slate-300 col-span-2">
-            <div className="mockup-browser-toolbar">
-              <div className="input">My Video</div>
-            </div>
-            {blob && (
-              <video
-                ref={videoRef}
-                onTimeUpdate={handleTimeChange}
-                controls
-                className="mt-4"
-              >
-                <source src={URL.createObjectURL(blob)} />
-              </video>
-            )}
-            <div className="overflow-y-auto h-[100px] my-7 px-8 rounded-md">
-              {Object.keys(timestamps).map((key) => {
-                return (
-                  <span
-                    key={key}
-                    id={`timestamp-${key}`}
-                    className={`${currKey == key ? "bg-yellow-200" : null}`}
-                    onClick={() => scrollToHighlightedElement(key)}
-                  >
-                    {timestamps[key].transcript}
-                  </span>
-                );
-              })}
-            </div>
+  return loading === 0 ? (
+    <div className="mt-10 flex flex-col items-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 w-11/12 p-4 mx-auto text-center">
+        <div className="mockup-browser border bg-slate-300 col-span-2">
+          <div className="mockup-browser-toolbar">
+            <div className="input">My Video</div>
           </div>
-
-          <div
-            className="ml-4 w-11/12 px-4 py-4 text-left bg-slate-300 rounded-lg h-fit"
-            style={{ alignSelf: "center" }}
-          >
-            <ScrollableComponent
-              data={feedbackList.map((e) => (
-                <div
-                  className={` mb-6${
-                    currKey && timestamps[currKey].feedback === e
-                      ? ".text-slate-950 font-bold text-xl"
-                      : "text-base font-normal"
-                  }`}
+          {blob && (
+            <video
+              ref={videoRef}
+              onTimeUpdate={handleTimeChange}
+              controls
+              className="mt-4"
+            >
+              <source src={URL.createObjectURL(blob)} />
+            </video>
+          )}
+          <div className="overflow-y-auto h-[100px] my-7 px-8 rounded-md">
+            {Object.keys(timestamps).map((key) => {
+              return (
+                <span
+                  key={key}
+                  id={`timestamp-${key}`}
+                  className={`${currKey == key ? "bg-yellow-200" : null}`}
+                  onClick={() => scrollToHighlightedElement(key)}
                 >
-                  {e}
-                </div>
-              ))}
-              containerId={"feedbackContainer"}
-            />
+                  {timestamps[key].transcript}
+                </span>
+              );
+            })}
           </div>
         </div>
-        <div class="w-11/12 bg-slate-100 p-4 border-4 border-slate-400 rounded-md mb-4 indicator">
-          <span className="indicator-item badge badge-accent">Summary</span>
-          <div>
-            <p className="font-semibold">{data.summary}</p>
-          </div>
+
+        <div
+          className="ml-4 w-11/12 px-4 py-4 text-left bg-slate-300 rounded-lg h-fit"
+          style={{ alignSelf: "center" }}
+        >
+          <BarGraph data={Object.entries(data.score)} />
+          <ScrollableComponent
+            data={feedbackList.map((e) => (
+              <div
+                className={` mb-6${
+                  currKey && timestamps[currKey].feedback === e
+                    ? ".text-slate-950 font-bold text-xl"
+                    : "text-base font-normal"
+                }`}
+              >
+                {e}
+              </div>
+            ))}
+            containerId={"feedbackContainer"}
+          />
         </div>
       </div>
-    ) : (
-      <BouncingDotsLoader />
-    );
-  }
+      <div class="w-11/12 bg-slate-100 p-4 border-4 border-slate-400 rounded-md mb-4 indicator">
+        <span className="indicator-item badge badge-accent">Summary</span>
+        <div>
+          <p className="font-semibold">{data.summary}</p>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <BouncingDotsLoader />
+  );
 }
